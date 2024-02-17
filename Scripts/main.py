7595f4ddd52cc2c507049fdbe2e0a46c2adeb690
CodeWizDurham <durham@thecodewiz.com>
2024-02-17 09:36:53 -0600

    Trees work now

--- Scripts/main.py
+++ Scripts/main.py
@@ -33,24 +33,34 @@ def main():
              pygame.Rect(random.randint(0, 450), random.randint(0, 300), 50, 50)]
 
     while True:
+        first_active = None
+        second_active = None
+        third_active = None
         screen.fill("gray")
         player_rect = pygame.Rect(x - (50 / 2), y - (50 / 2), 50, 50)
         pygame.draw.rect(screen, "blue", player_rect, border_radius=20)
         E_Button = PygE.image(["Assets", "E.png"], 0, (30, 30), pygame.Rect(0, 0, 50, 50))
+
         for i in range(0, 3, 1):
             pygame.draw.rect(screen, (0, 150, 0), trees[i])
+
         if player_rect.colliderect(trees[0]):
-            screen.blit(E_Button.image, (trees[0].x + 12, trees[0].y + 12))
-            first_active = True
+            if trees[0] != 0:
+                screen.blit(E_Button.image, (trees[0].x + 12, trees[0].y + 12))
+                first_active = True
         if player_rect.colliderect(trees[1]):
-            screen.blit(E_Button.image, (trees[1].x + 12, trees[1].y + 12))
-            second_active = True
+            if trees[1] != 0:
+                screen.blit(E_Button.image, (trees[1].x + 12, trees[1].y + 12))
+                second_active = True
         if player_rect.colliderect(trees[2]):
-            screen.blit(E_Button.image, (trees[2].x + 12, trees[2].y + 12))
-            third_active = True
+            if trees[2] != 0:
+                screen.blit(E_Button.image, (trees[2].x + 12, trees[2].y + 12))
+                third_active = True
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                quit() 
+                quit()
+
         key = pygame.key.get_pressed()
         if key[pygame.K_w]:
             y -= 0.1
@@ -62,9 +72,19 @@ def main():
             x += 0.1
         if key[pygame.K_s]:
             y += 0.1
+        if first_active == True:
+            if key[pygame.K_e]:
+                trees.pop(0)
+                trees.insert(1, pygame.Rect(-10, -10, 0, 0))
+        if second_active == True:
+            if key[pygame.K_e]:
+                trees.pop(0)
+                trees.insert(1, pygame.Rect(-10, -10, 0, 0))
+        if third_active == True:
+            if key[pygame.K_e]:
+                trees.pop(2)
+                trees.insert(2, pygame.Rect(-10, -10, 0, 0))
         pygame.display.update()
         
-        
-
 if __name__ == "__main__":
     main()
